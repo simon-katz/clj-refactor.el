1fbbe3418d76fd2c54e8f08d33cfea68b3d74af3
Benedek Fazekas <benedek.fazekas@gmail.com>
2014-05-04 12:00:24 +0100

    uses jvm analyzer

--- src/refactor_middleware/analyzer.clj
+++ src/refactor_middleware/analyzer.clj
@@ -1,59 +1,27 @@
 (ns refactor-middleware.analyzer
   (:refer-clojure :exclude [macroexpand-1 read read-string])
   (:require [clojure.tools.analyzer :as ana]
+            [clojure.tools.analyzer.jvm :as ana.jvm]
             [clojure.tools.analyzer.ast :refer :all]
-            [clojure.tools.analyzer.utils :refer [resolve-var]]
             [clojure.tools.nrepl.misc :refer [response-for]]
             [clojure.tools.nrepl.transport :as transport]
             [clojure.tools.nrepl.middleware :refer [set-descriptor!]]
             [clojure.tools.reader :as r])
   (:import java.io.PushbackReader))
 
-(defn desugar-host-expr [[op & expr :as form]]
-  (if (symbol? op)
-    (let [opname (name op)]
-      (cond
-       (= (first opname) \.) ; (.foo bar ..)
-       (let [[target & args] expr
-             args (list* (symbol (subs opname 1)) args)]
-         (with-meta (list '. target (if (= 1 (count args)) ;; we don't know if (.foo bar) ia
-                                      (first args) args)) ;; a method call or a field access
-           (meta form)))
-       (= (last opname) \.) ;; (class. ..)
-       (with-meta (list* 'new (symbol (subs opname 0 (dec (count opname)))) expr)
-         (meta form))
-       :else form))
-    form))
-
-(defn macroexpand-1 [form env]
-  (if (seq? form)
-    (let [op (first form)]
-      (if (ana/specials op)
-        form
-        (let [v (resolve-var op env)]
-          (if (and (not (-> env :locals (get op))) ;; locals cannot be macros
-                   (:macro (meta v)))
-            (apply v form env (rest form)) ; (m &form &env & args)
-            (desugar-host-expr form)))))
-        form))
-
 (def e (ana/empty-env))
 
 (defmacro ast [form]
-  `(binding [ana/macroexpand-1 macroexpand-1
-             ana/create-var    ~(fn [sym env]
-                                  (doto (intern (:ns env) sym)
-                                    (reset-meta! (meta sym))))
-             ana/parse         ana/-parse
+  `(binding [ana/macroexpand-1 ana.jvm/macroexpand-1
+             ana/create-var    ana.jvm/create-var
+             ana/parse         ana.jvm/parse
              ana/var?          ~var?]
      (ana/analyze '~form e)))
 
 (defmacro string-ast [string]
-  `(binding [ana/macroexpand-1 macroexpand-1
-             ana/create-var    ~(fn [sym env]
-                                  (doto (intern (:ns env) sym)
-                                    (reset-meta! (meta sym))))
-             ana/parse         ana/-parse
+  `(binding [ana/macroexpand-1 ana.jvm/macroexpand-1
+             ana/create-var    ana.jvm/create-var
+             ana/parse         ana.jvm/parse
              ana/var?          ~var?]
      (ana/analyze (r/read-string ~string) e)))
 
